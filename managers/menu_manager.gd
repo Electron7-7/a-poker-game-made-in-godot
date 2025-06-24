bb4fe195b65775cda98addb62bdf32c94a1d78fa
Electron7-7 <teahaile2@teahaile.com>
2025-06-24 19:34:08 -0400

    feature(implemented prototype "continue" button in main menu, using 'WorldManager'): Also, tweaked the theming, added some faux buttons, and started implemented a way for the main menu to also be the pause menu (which will have different buttons)

--- managers/menu_manager.gd
+++ managers/menu_manager.gd
@@ -2,8 +2,11 @@ class_name MenuManager extends Control
 
 static var main_menu : Control = preload("res://menus/main_menu.tscn").instantiate()
 static var settings_menu : Control = preload("res://menus/settings_menu.tscn").instantiate()
+static var server_menu : Control = preload("res://menus/server_menu.tscn").instantiate()
 
 func _activate_menu(menu : Control) -> void:
+    if(menu == main_menu):
+        menu.set_pause_menu(global_GameManager.IsGameRunning())
     menu.visible = true
 
 func _deactivate_menu(menu : Control) -> void:
@@ -21,8 +24,9 @@ func Init() -> void:
     add_child(main_menu)
     add_child(settings_menu)
     add_child(server_menu)
-    _solo_menu(main_menu)
+    main_menu.Init()
     settings_menu.load_settings()
+    _solo_menu(main_menu)
 
 func SwitchMenus(to: Control, from : Control = null) -> void:
     _activate_menu(to)
--- menus/main_menu.gd
+++ menus/main_menu.gd
@@ -1,12 +1,35 @@
 extends Control
 
+static var _is_pause_menu : bool = false
+static var _saves_toggle  : bool = false
+
+@onready var _hosting_buttons : HBoxContainer = $"%HostingButtons"
+@onready var _game_buttons    : HBoxContainer = $"%GameButtons"
+@onready var _saves           : ScrollContainer = $"%Saves"
+@onready var _save_games      : VBoxContainer = $"%SaveGames"
 @onready var _settings_button : Button = $"%Settings"
 @onready var _exit_button     : Button = $"%Exit"
+@onready var _quit_button     : Button = $"%Quit"
+@onready var _new_button      : Button = $"%New"
+@onready var _continue_button : Button = $"%Continue"
+@onready var _template_world  : Button = $"%_WorldTemplate"
 
-func _ready() -> void:
+func Toggled() -> void:
+    if(global_WorldManager.HasWorlds()):
+        _continue_button.disabled = false
+        _continue_button.focus_mode = Control.FOCUS_ALL
+    else:
+        _continue_button.disabled = true
+        _continue_button.focus_mode = Control.FOCUS_NONE
+        _saves.visible = false
+
+func Init() -> void:
+    _saves.visible = false
     _exit_button.pressed.connect(try_exit)
     _settings_button.pressed.connect(toggle_settings.bind(true))
+    _continue_button.pressed.connect(toggle_saves)
     global_MenuManager.settings_menu.close.connect(toggle_settings.bind(false))
+    self.visibility_changed.connect(Toggled)
 
 func try_exit() -> void:
     get_tree().quit()
@@ -16,3 +39,30 @@ func toggle_settings(toggle_on : bool) -> void:
         global_MenuManager.SwitchMenus(global_MenuManager.settings_menu, self)
         return
     global_MenuManager.SwitchMenus(self, global_MenuManager.settings_menu)
+
+func toggle_saves() -> void:
+    _saves_toggle = !_saves_toggle
+    _saves.visible = _saves_toggle
+    if(_saves_toggle):
+        for world in global_WorldManager.GetWorldNames():
+            var new_button : Button = _template_world.duplicate()
+            new_button.name = world
+            new_button.text = world
+            new_button.visible = true
+            new_button.theme_type_variation = &"WorldSelectButton"
+            new_button.pressed.connect(try_load_world.bind(world))
+            _save_games.add_child(new_button)
+    else:
+        for child in _save_games.get_children():
+            child.queue_free()
+    Toggled() # I just don't want to copy paste code and I'm lazy
+
+func try_load_world(world_name : String) -> Error:
+    return OK
+
+func set_pause_menu(is_pause_menu : bool) -> void:
+    _is_pause_menu = is_pause_menu
+    _quit_button.visible = _is_pause_menu
+    _hosting_buttons.visible = _is_pause_menu
+    _game_buttons.visible = !_is_pause_menu
+    _exit_button.visible = !_is_pause_menu
